69b3e635103f2567ea4d4cc759056f0a49260641
Sander Mak <sander.mak@luminis.eu>
2015-09-10 15:38:40 +0200

    A first small example for the new JigSaw early access build

---
+++ src/module1/com/test/TestClassModule1.java
@@ -0,0 +1,9 @@
+package com.test;
+
+import com.moretest.TestClassModule2;
+
+public class TestClassModule1 {
+   public static void main(String[] args) {
+     System.out.println("Hi from " + TestClassModule2.msg());
+   }
+}
---
+++ src/module2/com/moretest/TestClassModule2.java
@@ -0,0 +1,7 @@
+package com.moretest;
+
+public class TestClassModule2 {
+   public static String msg() {
+     return "from module 2!";
+   }
+}
